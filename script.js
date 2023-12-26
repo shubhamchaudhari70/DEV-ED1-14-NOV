4a614ff24fec723345649fa31757b8b8b2208397
Ankush Chaudhari <chaudhariankush70@gmail.com>
2023-12-26 21:01:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function beforesubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    //we need to convert String -->date(en_IN) means salesforce format of current user. you can fetch Salesforce local in dev console anonomus window UserIno.getLocale() method.
+
+}
